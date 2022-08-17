3de4e6fa00fd1070c5b2e6bf35a2117741d26520
Joao <joaoviniciusvitral@hotmail.com>
2022-08-17 17:48:06 -0300

    Feat: creating home

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Flex } from '@chakra-ui/react';
+import { Form, Map } from '../../components';
+
+export function Home() {
+  return (
+    <Flex>
+      <Form />
+      <Map />
+    </Flex>
+  );
+}
