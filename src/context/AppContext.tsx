e511c92fdffc5440f4ebe730eb4c4091e9e2f96b
Joao <joaoviniciusvitral@hotmail.com>
2022-08-18 13:22:03 -0300

    feat: context

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,22 +1,80 @@
 import React, {
-  createContext, useContext, useEffect, useState, useRef, useMemo,
+  createContext, useContext, useEffect, useState, useMemo,
 } from 'react';
+import { airportsReq } from '../services';
 
-const AppContext = createContext({});
+interface iAp {
+  icao: string
+  iata: string
+  name: string
+  shortName: string
+  municipalityName: string
+  location: {
+    lat: string
+    lon: string
+  }
+  countryCode: string
+}
+
+interface iValue {
+  ap1: iAp | null
+  ap2: iAp | null
+  searchedAirports : iAp[]
+}
+
+export const AppContext = createContext({});
 
 export function AppProvider({ children }: any) {
-  const [airports, setAirports] = useState([]);
+  const [searchedAirports, setSearchedAirports] = useState([]);
+  const [ap1, setAp1] = useState<iAp>(
+    {
+      icao: '',
+      iata: '',
+      name: '',
+      shortName: '',
+      municipalityName: '',
+      location: {
+        lat: '',
+        lon: '',
+      },
+      countryCode: '',
+    },
+  );
+  const [ap2, setAp2] = useState<iAp>(
+    {
+      icao: '',
+      iata: '',
+      name: '',
+      shortName: '',
+      municipalityName: '',
+      location: {
+        lat: '',
+        lon: '',
+      },
+      countryCode: '',
+    },
+  );
 
-  const value = useMemo(() => ({
-    airports,
-    setAirports,
-  }), []);
+  const value: iValue = useMemo(() => ({
+    ap1,
+    ap2,
+    setAp1,
+    setAp2,
+    searchedAirports,
+    setSearchedAirports,
+  }), [
+    ap1,
+    ap2,
+    setAp1,
+    setAp2,
+    searchedAirports,
+    setSearchedAirports,
+  ]);
 
   return (
     <AppContext.Provider value={value}>
       {children}
     </AppContext.Provider>
-
   );
 }
 
